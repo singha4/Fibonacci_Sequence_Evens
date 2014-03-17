ef32ec85f2b5a291a66dca81fc4237694cda935a
singha4 <amandeep159632478@gmail.com>
2014-03-17 09:32:34 -0400

    version 1

---
+++ Fibonacci_Sequence_Evens/Fibonacci_Sequence_Evens/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci_Sequence_Evens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1, ab = 2;
+
+            while (ab <= 4000000)
+            {
+                ab = a + b;
+                a = b;
+                b = ab;
+                if (ab % 2 == 0)
+                {
+                    Console.WriteLine(ab);
+                }
+            }
+        }
+    }
+}
